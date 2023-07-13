c1b1836d62575844db246402002a417165aef1a8
Ozan <ozancangu24@gmail.com>
2023-07-13 16:55:55 +0300

    -Data observed and ui updated

--- app/src/main/java/com/ozancanguz/books/ui/fragments/booklist/BookListFragment.kt
+++ app/src/main/java/com/ozancanguz/books/ui/fragments/booklist/BookListFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ozancanguz.books.data.adapter.BookListAdapter
 import com.ozancanguz.books.databinding.FragmentBookListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,16 +18,40 @@ class BookListFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    // init adapter
+    private val adapter=BookListAdapter()
+
+    // init viewmodel
+    private val bookListViewModel:BookListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentBookListBinding.inflate(inflater, container, false)
 
+
+        // setting up recyclerview
+        setupRv()
+
+        // observe live data and update ui
+        observeLiveData()
+
         return binding.root
 
+    }
+
+    private fun observeLiveData() {
+        bookListViewModel.getBookList()
+        bookListViewModel.booklist.observe(viewLifecycleOwner, Observer {
+            adapter.setData(it)
+        })
+    }
+
+    private fun setupRv() {
+        binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter=adapter
     }
 
 
