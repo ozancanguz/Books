c2dd6d92511fa2df7e9e214e2190f543553dc134
Ozan <ozancangu24@gmail.com>
2023-07-13 16:05:30 +0300

    -Dagger hilt added

--- app/src/main/java/com/ozancanguz/books/ui/fragments/bookdetails/BookDetailsFragment.kt
+++ app/src/main/java/com/ozancanguz/books/ui/fragments/bookdetails/BookDetailsFragment.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ozancanguz.books.R
+import com.ozancanguz.books.databinding.FragmentBookDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class BookDetailsFragment : Fragment() {
+    private var _binding: FragmentBookDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
 
 
     override fun onCreateView(
@@ -16,7 +22,11 @@ class BookDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false)
+        _binding = FragmentBookDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+
     }
 
 
