0e6c04260739c4802cfb3c8174785d430a04c9b3
Ozan <ozancangu24@gmail.com>
2023-07-13 16:09:24 +0300

    -Data classes created from api

---
+++ app/src/main/java/com/ozancanguz/books/data/model/Book.kt
@@ -0,0 +1,6 @@
+package com.ozancanguz.books.data.model
+
+data class Book(
+    val result: List<Result>,
+    val success: Boolean
+)
---
+++ app/src/main/java/com/ozancanguz/books/data/model/Result.kt
@@ -0,0 +1,11 @@
+package com.ozancanguz.books.data.model
+
+data class Result(
+    val fiyat: String,
+    val image: String,
+    val indirim: String,
+    val title: String,
+    val url: String,
+    val yayın: String,
+    val yazar: String
+)
